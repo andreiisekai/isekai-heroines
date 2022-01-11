6bf5541836be7e8c152adeccf695f492d70a659e
internetkun <internetkundev@gmail.com>
2022-01-11 14:59:25 +0200

    Text Mesh Pro Label on Cube prefab

--- Isekai Heroines/Assets/Scripts/CubeEditor.cs
+++ Isekai Heroines/Assets/Scripts/CubeEditor.cs
@@ -1,32 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
     [Range(1f, 20f)]
     [SerializeField] float gridSize = 10f;
+
+    TextMeshPro textMeshPro;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        textMeshPro = GetComponentInChildren<TextMeshPro>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = getSnapPosition();
+        SnapPosition();
     }
 
-    Vector3 getSnapPosition()
+    void SnapPosition()
     {
         Vector3 snapPos;
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.y = Mathf.RoundToInt(transform.position.y / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
 
-        return snapPos;
+        transform.position = snapPos;
+        textMeshPro.text = snapPos.x/gridSize + "," + snapPos.z/gridSize;
     }
 }
